064de8f75a676fcb8727e4218caff1b1f3b22d37
fiery.snowflake <extinctCoder@outlook.com>
2024-08-11 00:07:37 +0600

    chore: initial project bootstrap completed

--- src/main.py
+++ src/main.py
@@ -1,48 +1,8 @@
-import uvicorn
-from fastapi import Body, FastAPI
-from pydantic import BaseModel, Field
+from fastapi import FastAPI, Header
 
 app = FastAPI()
 
 
-class Blog(BaseModel):
-    title: str
-    body: str
-    published: bool = True
-
-
-@app.get("/blog")
-def create_blog(blog: Blog):
-    return {"data": blog}
-
-
-@app.post("/blog")
-def create_blog(blog: Blog):
-    return {"data": f"Blog is created with title as {blog.title}"}
-
-
-@app.get("/blog")
-def index(limit=10, published: bool = True, sort: str | None = None):
-    if published:
-        return {"data": f"{limit} published blogs from the db"}
-    else:
-        return {"data": f"{limit} blogs from the db"}
-
-
-@app.get("/blog/unpublished")
-def unpublished():
-    return {"data": "All unpublished blogs"}
-
-
-@app.get("/blog/{id}")
-def show(id: int):
-    return {"data": id}
-
-
-@app.get("/blog/{id}/comments")
-def comments(id: int):
-    return {"data": {"1", "2"}}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
